432130223d42108899656d22996d188883eb20e3
Le Qui Tu <21521609@gm.uit.edu.vn>
2023-12-28 14:22:38 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace practice_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateFile = new System.Windows.Forms.Button();
+            this.btnCalcSum = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDoSomthing = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreateFile
+            // 
+            this.btnCreateFile.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateFile.Location = new System.Drawing.Point(119, 195);
+            this.btnCreateFile.Name = "btnCreateFile";
+            this.btnCreateFile.Size = new System.Drawing.Size(147, 81);
+            this.btnCreateFile.TabIndex = 0;
+            this.btnCreateFile.Text = "Tạo file";
+            this.btnCreateFile.UseVisualStyleBackColor = true;
+            this.btnCreateFile.Click += new System.EventHandler(this.btnCreateFile_Click);
+            // 
+            // btnCalcSum
+            // 
+            this.btnCalcSum.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcSum.Location = new System.Drawing.Point(510, 195);
+            this.btnCalcSum.Name = "btnCalcSum";
+            this.btnCalcSum.Size = new System.Drawing.Size(140, 78);
+            this.btnCalcSum.TabIndex = 1;
+            this.btnCalcSum.Text = "Đọc file và tính tổng";
+            this.btnCalcSum.UseVisualStyleBackColor = true;
+            this.btnCalcSum.Click += new System.EventHandler(this.btnCalcSum_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F);
+            this.label1.Location = new System.Drawing.Point(199, 344);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tổng = ";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Times New Roman", 16.2F);
+            this.txtResult.Location = new System.Drawing.Point(307, 344);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(256, 39);
+            this.txtResult.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(625, 46);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Đọc file và tính tổng với Multi Thread";
+            // 
+            // btnDoSomthing
+            // 
+            this.btnDoSomthing.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoSomthing.Location = new System.Drawing.Point(307, 484);
+            this.btnDoSomthing.Name = "btnDoSomthing";
+            this.btnDoSomthing.Size = new System.Drawing.Size(167, 56);
+            this.btnDoSomthing.TabIndex = 5;
+            this.btnDoSomthing.Text = "Đổi màu nền";
+            this.btnDoSomthing.UseVisualStyleBackColor = true;
+            this.btnDoSomthing.Click += new System.EventHandler(this.btnDoSomthing_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(163, 406);
+            this.label3.MaximumSize = new System.Drawing.Size(500, 0);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(10);
+            this.label3.Size = new System.Drawing.Size(496, 64);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Khi dùng multi thread, ta có thể làm việc khác khi đang tính tổng, ví dụ đổi màu " +
+    "nền...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 566);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDoSomthing);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcSum);
+            this.Controls.Add(this.btnCreateFile);
+            this.Name = "Form1";
+            this.Text = "Multi Thread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateFile;
+        private System.Windows.Forms.Button btnCalcSum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDoSomthing;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Numerics;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace practice_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private readonly int NUM_OF_ELEMENTS = 1000000;
+
+        private void btnCreateFile_Click(object sender, System.EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text Files|*.txt|All Files|*.*";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+
+                    btnCalcSum.Enabled = false;
+
+                    try
+                    {
+                        using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                        {
+                            using (StreamWriter sw = new StreamWriter(fs))
+                            {
+                                Random random = new Random();
+                                for (int i = 0; i < NUM_OF_ELEMENTS; i++)
+                                {
+                                    int ranNum = random.Next();
+                                    sw.Write(ranNum);
+                                    if (i < NUM_OF_ELEMENTS - 1)
+                                    {
+                                        sw.Write(' ');
+                                    }
+                                }
+                            }
+                        }
+
+                        MessageBox.Show("Tạo file với 1 triệu số nguyên ngẫu nhiên thành công", "Tạo file", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        btnCalcSum.Enabled = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+        }
+
+
+
+        private BigInteger CalculateSumFromFile(string filePath)
+        {
+            BigInteger sum = BigInteger.Zero;
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader streamReader = new StreamReader(fileStream))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        string line = streamReader.ReadLine();
+
+                        string[] items = line.Split(' ');
+
+                        foreach (string item in items)
+                        {
+                            if (BigInteger.TryParse(item, out BigInteger value))
+                            {
+                                sum += value;
+                            }
+                        }
+                    }
+                }
+            }
+            return sum;
+        }
+
+        private void EnableControls(bool enable)
+        {
+            if (btnCreateFile.InvokeRequired)
+            {
+                btnCreateFile.Invoke((MethodInvoker)delegate
+                {
+                    btnCreateFile.Enabled = enable;
+                });
+            }
+            else
+            {
+                btnCreateFile.Enabled = enable;
+            }
+
+            if (btnCalcSum.InvokeRequired)
+            {
+                btnCalcSum.Invoke((MethodInvoker)delegate
+                {
+                    btnCalcSum.Enabled = enable;
+                });
+            }
+            else
+            {
+                btnCalcSum.Enabled = enable;
+            }
+        }
+
+        private void btnCalcSum_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    EnableControls(false);
+                    txtResult.Text = "Đang tính...";
+
+                    Thread thread = new Thread((param) =>
+                    {
+                        string threadFilePath = (string)param;
+
+                        try
+                        {
+                            BigInteger sum = CalculateSumFromFile(threadFilePath);
+
+                            txtResult.Invoke((MethodInvoker)delegate
+                            {
+                                txtResult.Text = sum.ToString();
+                            });
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error reading file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        finally
+                        {
+                            EnableControls(true);
+                        }
+                    });
+
+                    thread.IsBackground = true;
+
+                    thread.Start(filePath);
+                }
+            }
+
+        }
+
+
+        private void btnDoSomthing_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            int red = random.Next(256);
+            int green = random.Next(256);
+            int blue = random.Next(256);
+
+            Color randomColor = Color.FromArgb(red, green, blue);
+
+            this.BackColor = randomColor;
+            this.BackColor = randomColor;
+        }
+    }
+}
